521b4aff1aa80aa2b9538d7d4f77dc2a93bf9864
doktor_mm <mmitreski8021rn@raf.rs>
2024-01-12 20:41:31 +0100

    Initial commit

---
+++ src/main/java/raf/sk/drugiprojekat/korisnickiservis/KorisnickiServisApplication.java
@@ -0,0 +1,13 @@
+package raf.sk.drugiprojekat.korisnickiservis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorisnickiServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorisnickiServisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raf/sk/drugiprojekat/korisnickiservis/KorisnickiServisApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.sk.drugiprojekat.korisnickiservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorisnickiServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
